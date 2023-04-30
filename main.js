0edc80810803eb68cdefe8f9d5824175ae796176
Widenyo <misterhomunculo@gmail.com>
2023-04-30 03:30:32 -0300

    GPT RESPONDE EN BASE A LA PERSONALIDAD DEL PERSONAJE

--- main.js
+++ main.js
@@ -1,9 +1,58 @@
-const fs = require('fs');
+require('dotenv').config()
+
+const fs = require('fs')
 
 const API_KEY = process.env.OPENAI_KEY;
+
+const OpenAIService = require('./microservices/openai')
 
 const charConfigFile = fs.readFileSync('./config/char_config.json')
 let char = JSON.parse(charConfigFile)
 
 
-console.log(char)
+const main_prompt = `From now on, you are going to roleplay as ${char.name}`
+
+const newChatMsg = {role: 'system', content: "[Start a new chat]"}
+
+const mappedExampleMessages = char.dialogue_examples.map(exampleConv => {
+    const mappedConv = exampleConv.map((m) => {
+        return {
+            role: m.includes('{name}') ? 'assistant' : 'user',
+            content: m.split(': ')[1],
+            //El name está comentado porque cuando incluyo los nombres de los users de ejemplo me tira error la req.
+            //Creo que el campo de name es un peligro porque si alguien tiene un espacio en el nombre o un caracter especial tira bad req.
+            // name: m.includes('{name}') ? 'Kabachi' : m.split(':')[0]
+        }
+    })
+
+    console.log(mappedConv)
+
+    const newConv = [...mappedConv, newChatMsg]
+    return newConv
+}).flat(2)
+
+
+const wholeInitialPrompt = `
+    ${main_prompt}\n
+    ${replaceCharName(char.description)}\n
+    Circumstances and context of the dialogue: ${char.scenario}
+`
+
+const initialMessages = [{role: 'system', content: wholeInitialPrompt}, 
+    ...mappedExampleMessages, 
+    {role: 'system', content: `Remember that ${char.name}'s personality is: ${char.personality}`},
+    newChatMsg]
+
+    console.log(initialMessages)
+
+const openAIService = new OpenAIService(API_KEY)
+
+openAIService.promptRequest("andate a cagar forra " + char.name, initialMessages, {role: 'user', name: "jorge2002"}).then(r => {
+    console.log(r.data.choices[0].message.content)
+}).catch(e => {
+    console.log(e)
+})
+
+function replaceCharName(str){
+    return str.replaceAll("{name}", char.name)
+}
---
+++ microservices/openai.js
@@ -0,0 +1,31 @@
+const { Configuration, OpenAIApi } = require('openai')
+
+class openAIService{
+    constructor(API_KEY){
+        this.API_KEY = API_KEY
+        this.config = new Configuration({
+            apiKey: this.API_KEY
+        }),
+        this.client = new OpenAIApi(this.config);
+    }
+
+    async promptRequest(prompt, messages = [], options = {}){
+        const newMessage = {
+            role: options.role || 'user',
+            content: prompt,
+            name: options.name || null
+        }
+        const allMessages = [...messages, newMessage]
+        console.log(allMessages)
+            const response = await this.client.createChatCompletion({
+                messages: allMessages,
+                temperature: options.temperature || 0.70777,
+                model: "gpt-3.5-turbo",
+                max_tokens: 2000
+            });
+            return response
+    }
+
+}
+
+module.exports = openAIService
