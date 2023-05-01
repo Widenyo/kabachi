d2d286a32a3ae8fa463dfe655eba008006cae6a3
loops6340 <71669105+loops6340@users.noreply.github.com>
2023-05-01 10:57:56 -0500

    código un poco más limpio

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 require('dotenv').config()
- // index.js
+// index.js
 
 const fs = require('fs')
+
+//config
 const charConfigFile = fs.readFileSync('./config/char_config.json')
 const streamConfigFile = fs.readFileSync('./config/stream_config.json')
 const ttsConfigFile = fs.readFileSync('./config/tts_config.json')
@@ -9,39 +11,46 @@ const stream_config = JSON.parse(streamConfigFile)
 const tts_config = JSON.parse(ttsConfigFile)
 
 const API_KEY = process.env.OPENAI_KEY;
+const openAIService = new OpenAIService(API_KEY)
 
-const player = require('node-wav-player');
+const liveChat = new LiveChat({ liveId: stream_config.stream_id }, stream_config.time)
 
+
+//microservices
 const OpenAIService = require('./microservices/openai');
+const LiveChat = require("./microservices/livechat")
+
+//tts
+const player = require('node-wav-player');
 const ttsSdk = require('microsoft-cognitiveservices-speech-sdk');
 const speechConfig = ttsSdk.SpeechConfig.fromSubscription(process.env.TTS_KEY, process.env.TTS_REGION);
 speechConfig.speechSynthesisVoiceName = tts_config.voiceName;
 const audiocfg = ttsSdk.AudioConfig.fromAudioFileOutput(tts_config.outputName);
-let synthesizer = new ttsSdk.SpeechSynthesizer(speechConfig, audiocfg);
- 
+const synthesizer = new ttsSdk.SpeechSynthesizer(speechConfig, audiocfg);
 
-let char = JSON.parse(charConfigFile, audiocfg)
+
+const char = JSON.parse(charConfigFile, audiocfg)
 
 
 const main_prompt = `From now on, you are going to roleplay as ${char.name}`
 
-const newChatMsg = {role: 'system', content: "[Start a new chat]"}
+const newChatMsg = { role: 'system', content: "[Start a new chat]" }
 
 const mappedExampleMessages = char.dialogue_examples.map(exampleConv => {
-    const mappedConv = exampleConv.map((m) => {
-        return {
-            role: m.includes('{name}') ? 'assistant' : 'user',
-            content: m.split(': ')[1],
-            //El name está comentado porque cuando incluyo los nombres de los users de ejemplo me tira error la req.
-            //Creo que el campo de name es un peligro porque si alguien tiene un espacio en el nombre o un caracter especial tira bad req.
-            // name: m.includes('{name}') ? 'Kabachi' : m.split(':')[0]
-        }
-    })
+  const mappedConv = exampleConv.map((m) => {
+    return {
+      role: m.includes('{name}') ? 'assistant' : 'user',
+      content: m.split(': ')[1],
+      //El name está comentado porque cuando incluyo los nombres de los users de ejemplo me tira error la req.
+      //Creo que el campo de name es un peligro porque si alguien tiene un espacio en el nombre o un caracter especial tira bad req.
+      // name: m.includes('{name}') ? 'Kabachi' : m.split(':')[0]
+    }
+  })
 
-    console.log(mappedConv)
+  console.log(mappedConv)
 
-    const newConv = [...mappedConv, newChatMsg]
-    return newConv
+  const newConv = [...mappedConv, newChatMsg]
+  return newConv
 }).flat(2)
 
 
@@ -51,147 +60,65 @@ const wholeInitialPrompt = `
     Circumstances and context of the dialogue: ${char.scenario}
 `
 
-const initialMessages = [{role: 'system', content: wholeInitialPrompt}, 
-    ...mappedExampleMessages, 
-    {role: 'system', content: `Remember that ${char.name}'s personality is: ${char.personality}`},
-    newChatMsg]
+const initialMessages = [{ role: 'system', content: wholeInitialPrompt },
+...mappedExampleMessages,
+{ role: 'system', content: `Remember that ${char.name}'s personality is: ${char.personality}` },
+  newChatMsg]
 
 const allMessages = [...initialMessages]
 
-const openAIService = new OpenAIService(API_KEY)
 
-const { fetchChat, fetchLivePage } = require("youtube-chat/dist/requests")
-const { EventEmitter } = require("events");
-
- class LiveChat extends EventEmitter {
- 
-  constructor(id, interval) {
-    super()
-    if (!id || (!("channelId" in id) && !("liveId" in id) && !("handle" in id))) {
-      throw TypeError("Required channelId or liveId or handle.")
-    } else if ("liveId" in id) {
-      this.liveId = id.liveId
-    }
-
-    this.id = id
-    this.interval = interval
-    this.lastMessage = ""
-  }
-
-  resetObserver(){
-    this.observer = setTimeout(() => this.#execute(), this.interval)
-  }
-
-  async start() {
-    if (this.observer) {
-      return false
-    }
-    try {
-      const options = await fetchLivePage(this.id)
-      this.liveId = options.liveId
-      this.options = options
-
-      this.observer = setTimeout(() => this.#execute(), this.interval)
-
-      this.emit("start", this.liveId)
-      return true
-    } catch (err) {
-      this.emit("error", err)
-      return false
-    }
-  }
-
-  stop(reason) {
-    if (this.observer) {
-      clearInterval(this.observer)
-      this.observer = undefined
-      this.emit("end", reason)
-    }
-  }
-
-  async #execute() {
-    if (!this.options) {
-      const message = "Not found options"
-      this.emit("error", new Error(message))
-      this.stop(message)
-      return
-    }
-
-    try {
-      const [chatItems, continuation] = await fetchChat(this.options)
-   
-      //console.log(chatItems)
-  
-      //chatItems.forEach((chatItem) => this.emit("chat", chatItem))
-      const lastChatItem = chatItems.at(-1)
-      if (lastChatItem) this.emit("chat", lastChatItem)
-      else{
-        console.log("STREAM MUERTO XD")
-        this.resetObserver()
-      }
-      this.options.continuation = continuation
-   //   console.log({continuation})
-    } catch (err) {
-      this.emit("error", err)
-    }
-  }
-}
-
-
-
-const liveChat = new LiveChat({liveId: stream_config.stream_id}, stream_config.time)
-
-const main = async () => {
 
 liveChat.on("start", (liveId) => {
-  console.log({liveId})
+  console.log({ liveId })
 })
 
 
 liveChat.on("end", (reason) => {
-    console.log(reason)
+  console.log(reason)
 })
 
 
 liveChat.on("chat", async (chatItem) => {
-    const message = `${chatItem.author.name}: ` + chatItem.message[0].text
-    if(!(this.lastMessage === message && message.includes("undefined"))){
-        try{
-            const promptResReq = await openAIService.promptRequest(message, allMessages, {role: "user"})
-            const res = promptResReq.data.choices[0].message.content
-            newMessageAndReply(message, res)
-            console.log(message + '\n', `${char.name}: ${res}`)
-                // Start the synthesizer and wait for a result.
-                synthesizer.speakTextAsync(res,
-                    async (result) => {
-                  if (result.reason === ttsSdk.ResultReason.SynthesizingAudioCompleted) {
-                    console.log('Sintetizado')
-                    synthesizer.close();
-                    await player.play({path: tts_config.outputName, sync: true})
-                    console.log('Finished playing audio.')
-                  } else {
-                    console.error("Speech synthesis canceled, " + result.errorDetails +
-                        "\nDid you set the speech resource key and region values?");
-                        synthesizer.close();
-                  }
-                  liveChat.resetObserver()
-                  synthesizer = new ttsSdk.SpeechSynthesizer(speechConfig);
-                },
-                     (err) => {
-                  console.trace("err - " + err);
-                  liveChat.resetObserver()
-                  synthesizer.close();
-                  synthesizer = new ttsSdk.SpeechSynthesizer(speechConfig);
-                });
-                console.log("Now synthesizing");
-        }catch(e){
-            console.error(e.message)
-            liveChat.resetObserver();
-        }
-        this.lastMessage = message
+  const message = `${chatItem.author.name}: ${chatItem.message[0].text}`
+
+  if (!(this.lastMessage === message && message.includes("undefined"))) {
+    try {
+      const promptResReq = await openAIService.promptRequest(message, allMessages, { role: "user" })
+      const res = promptResReq.data.choices[0].message.content
+      newMessageAndReply(message, res)
+      console.log(message + '\n', `${char.name}: ${res}`)
+      // Start the synthesizer and wait for a result.
+      synthesizer.speakTextAsync(res,
+        async (result) => {
+          if (result.reason === ttsSdk.ResultReason.SynthesizingAudioCompleted) {
+            console.log('Sintetizado')
+            synthesizer.close();
+            await player.play({ path: tts_config.outputName, sync: true })
+            console.log('Finished playing audio.')
+          } else {
+            console.error("Speech synthesis canceled, " + result.errorDetails +
+              "\nDid you set the speech resource key and region values?");
+            synthesizer.close();
+          }
+          liveChat.resetObserver()
+          synthesizer = new ttsSdk.SpeechSynthesizer(speechConfig);
+        },
+        (err) => {
+          console.trace("err - " + err);
+          liveChat.resetObserver()
+          synthesizer.close();
+          synthesizer = new ttsSdk.SpeechSynthesizer(speechConfig);
+        });
+      console.log("Now synthesizing");
+    } catch (e) {
+      console.error(e.message)
+      liveChat.resetObserver();
     }
-    })
-}
+    this.lastMessage = message
+  }
+})
+
 
 liveChat.on("error", (err) => {
 
@@ -200,24 +127,20 @@ liveChat.on("error", (err) => {
 
 
 liveChat.start().catch(e => {
-    console.log(e)
+  console.log(e)
 })
 
-main()
 
+const replaceCharName = (str) => str.replaceAll("{name}", char.name)
 
-function replaceCharName(str){
-    return str.replaceAll("{name}", char.name)
-}
-
-function newMessageAndReply(message, res){
-    allMessages.push(...[{
-        role: 'user',
-        content: message
-    },
-    {
-        content: res,
-        role: 'assistant',
-    }]
-    )
+const newMessageAndReply = (message, res) => {
+  allMessages.push(...[{
+    role: 'user',
+    content: message
+  },
+  {
+    content: res,
+    role: 'assistant',
+  }]
+  )
 }
