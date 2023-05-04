774cac1c8a33ebde247f06289b44d03cd0dcdb73
loops6340 <71669105+loops6340@users.noreply.github.com>
2023-05-03 20:03:46 -0500

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,7 +1,5 @@
 const fs = require("fs")
 
 module.exports = {
-    getTTSConfig: () => JSON.parse(fs.readFileSync("./config/tts_config.json")),
-    getStreamConfig: () => JSON.parse(fs.readFileSync("./config/stream_config.json")),
-    getCharConfig: () => fs.readFileSync("./config/char_config.json")
+    parseJSONFile: (path) => JSON.parse(fs.readFileSync(path))
 }
